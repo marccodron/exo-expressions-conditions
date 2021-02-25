7a2f305413f785d5a46ff984ccbfb1cb333b24de
marccodron <logs@marc-codron.fr>
2021-02-25 19:29:24 +0100

    add exo

---
+++ vote.js
@@ -0,0 +1,24 @@
+//Réécrire le programme suivant avec un ou deux ternary operator:
+
+//let age = 17
+//let canVote = false
+//if (age >= 18) {
+//  canVote = true
+//} else {
+//  canVote = false
+//}
+//if (canVote) {
+//  console.log('You can vote')
+//} else {
+//  console.log('You can not vote')
+//}
+
+
+
+let age = 23
+if (age >= 18) {
+ console.log('You are eligible to vote')
+}
+else {
+ console.log('You may not be eligible to vote')
+}
